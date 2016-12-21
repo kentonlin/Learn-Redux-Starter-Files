fac9b44696e3885cea3cca7536a588746e214474
Kenton <kentonlin1@gmail.com>
2016-12-21 10:51:29 -0500

    changes to reducers and store

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -13,6 +13,8 @@ const defaultState = {
   comments
 }
 
-export default const store = createStore(rootReducer, defaultState);
+const store = createStore(rootReducer, defaultState);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store; 
