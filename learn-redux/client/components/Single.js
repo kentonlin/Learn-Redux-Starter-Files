a702b0db9a97e619569ceede94e0f92cd8e468e2
Kenton <kentonlin1@gmail.com>
2016-12-29 21:19:58 -0500

    pass comment props from single to comments component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Photo from './Photo';
+import Comments from './Comments';
 // import comments
 
 const Single = React.createClass({
   render(){
     // index of the post
     // get us the post
-    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postId);
     console.log('this is the index from single.js',i);
     const post = this.props.posts[i];
     console.log('this is the post from single.js', post);
+    const postComments = this.props.comments[postId] || [];
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
+        <Comments postComments= { postComments }/>
       </div>
     );
   }
